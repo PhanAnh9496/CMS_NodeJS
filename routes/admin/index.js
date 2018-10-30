2af24172d961c34d8d163eafdc519dd198dde134
PhanAnh9496 <phananh9496@gmail.com>
2018-10-30 21:54:28 +0700

    CMS dummy data

--- routes/admin/index.js
+++ routes/admin/index.js
@@ -1,5 +1,7 @@
 var express = require('express');
 var router = express.Router();
+var faker = require('Faker');
+const Post = require('../../models/Post');
 
 router.all('/*', (req, res, next) => {
 	req.app.locals.layout = 'admin';
@@ -8,6 +10,22 @@ router.all('/*', (req, res, next) => {
 
 router.get('/', function (req, res, next) {
 	res.render('admin/index');
+});
+
+router.post('/generate-fake-posts', (req, res) => {
+	for(let i=0; i < req.body.amount; i++){
+		var post = new Post();
+		post.title = faker.Internet.userName();
+		post.status = 'public';
+		post.allowComments = true;
+		post.body = faker.Lorem.paragraphs();
+
+		post.save(function(err) {
+			if(err) throw err;
+		});
+		
+	}
+	res.redirect('/admin/posts');
 });
 
 module.exports = router;
