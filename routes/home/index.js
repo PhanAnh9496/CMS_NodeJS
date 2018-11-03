746717879cdd174a4b9687942022954f54ab0f5d
PhanAnh9496 <phananh9496@gmail.com>
2018-11-03 17:15:28 +0700

    Update_2  03/11/18

--- routes/home/index.js
+++ routes/home/index.js
@@ -2,6 +2,8 @@ var express = require('express');
 var router = express.Router();
 const Post = require('../../models/Post');
 const Category = require('../../models/Category');
+const User = require('../../models/User');
+const bcrypt = require('bcryptjs');
 
 router.all('/*', (req, res, next) => {
 	req.app.locals.layout = 'home';
@@ -11,7 +13,6 @@ router.all('/*', (req, res, next) => {
 router.get('/', function (req, res, next) {
 	Post.find({})
 		.then((posts) => {
-
 			Category.find({})
 				.then(categories => {
 					res.render('home/index', {
@@ -19,8 +20,6 @@ router.get('/', function (req, res, next) {
 						categories: categories
 					});
 				});
-
-
 		});
 
 });
@@ -32,9 +31,71 @@ router.get('/login', function (req, res, next) {
 	res.render('home/login');
 });
 
+//Register
 router.get('/register', function (req, res, next) {
 	res.render('home/register');
 });
+router.post('/register', function (req, res, next) {
+
+	let errors = [];
+
+	if (!req.body.firstName) {
+		errors.push({
+			message: 'Bạn chưa nhập tên!'
+		});
+	}
+
+	if (!req.body.lastName) {
+		errors.push({
+			message: 'Bạn chưa nhập họ!'
+		});
+	}
+	if (!req.body.email) {
+		errors.push({
+			message: 'Bạn chưa nhập email!'
+		});
+	}
+	if (!req.body.password) {
+		errors.push({
+			message: 'Bạn chưa nhập password!'
+		});
+	}
+	if (!req.body.passwordConfirm) {
+		errors.push({
+			message: 'Bạn chưa xác thực password!'
+		});
+	}
+	if (req.body.password !== req.body.passwordConfirm) {
+		errors.push({
+			message: 'Xác thực lại password!!!'
+		});
+	}
+	if (errors.length > 0) {
+		res.render('home/register', {
+			errors: errors
+		});
+	} else {
+
+		const newUser = new User({
+			firstName: req.body.firstName,
+			lastName: req.body.lastName,
+			email: req.body.email,
+			password: req.body.password
+		});
+
+		bcrypt.genSalt(10, function (err, salt) {
+			bcrypt.hash(newUser.password, salt, function (err, hash) {
+				newUser.password = hash;
+				newUser.save()
+					.then(savedUser => {
+						req.flash('success_message','Đăng ký thành công mời bạn Đăng Nhập');
+						res.redirect('/login');
+					});
+			});
+		});
+	}
+});
+
 
 router.get('/post/:id', function (req, res, next) {
 	Post.findOne({
